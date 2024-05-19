f95ad0c23af93ef96a8ae0521463121cceaf8cbd
bhargavchintha <bhargavchintha5588@gmail.com>
2024-05-19 17:18:28 +0530

    ok

--- Script/Ifamesreload.js
+++ Script/Ifamesreload.js
@@ -1,4 +1,4 @@
-function closeTab() {
+function closeTabfun() {
     // Reload the iframe by setting its source URL to the current source URL
     var iframe = document.getElementById("webPageFrame");
     iframe.src = iframe.src;
